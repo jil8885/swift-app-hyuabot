5b7380fb6d8b9d2afeff1dc056862dbd39003542
Jeongin Lee <jil8885@hanyang.ac.kr>
2021-07-04 11:36:23 +0900

    create swift ui project

---
+++ Shared/hyuabotApp.swift
@@ -0,0 +1,17 @@
+//
+//  hyuabotApp.swift
+//  Shared
+//
+//  Created by Jeongin on 2021/07/04.
+//
+
+import SwiftUI
+
+@main
+struct hyuabotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
